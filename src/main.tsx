26c194afc70fab4f6591c961e2e854a442bd7073
NARARIA03 <hyeonseong0305@gmail.com>
2024-09-25 12:21:01 +0900

    Chore: react-router-dom 초기 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import Router from "@router/Router";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>
 );
---
+++ src/router/Router.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Router(): React.JSX.Element {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
