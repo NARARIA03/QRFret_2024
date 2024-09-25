7988aed04f2e328a95e6d67a0770166e0af0ddb3
NARARIA03 <hyeonseong0305@gmail.com>
2024-09-25 12:04:38 +0900

    Initial commit
    React, Vite, TypeScript, Yarn 프로젝트 생성

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@assets", replacement: path.resolve(__dirname, "src/assets") },
+      {
+        find: "@components",
+        replacement: path.resolve(__dirname, "src/components"),
+      },
+      { find: "@hooks", replacement: path.resolve(__dirname, "src/hooks") },
+      { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
+      { find: "@router", replacement: path.resolve(__dirname, "src/router") },
+    ],
+  },
+});
